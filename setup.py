e534c2848c437761d29c406c89f9249f8c6db68e
GAI <67356536+GAI-313@users.noreply.github.com>
2024-10-21 17:51:17 +0900

    Ver 0.0.12

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 setup(
     name="serch_pharmaceuticals",
     description="measure serch pharmaceuticals interval time",
-    version="0.0.1",
+    version="0.0.12",
     author="GAI-313",
     author_email="nakartogawa.drone@gmail.com",
 
